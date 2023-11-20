f22043a23546967960305398f8db85d47efcedd2
Alenin Andrew <aleninmailbox@gmail.com>
2023-11-20 12:49:10 +0300

    add 4.2 edit 4.1

---
+++ src/test/java/api/PowerBank/tests/EP4_1/Ep4_2.java
@@ -0,0 +1,141 @@
+package api.PowerBank.tests.EP4_1;
+
+import api.PowerBank.ApiHelp.CardService.CardProductInfo;
+import api.PowerBank.ApiHelp.CardService.CardProductsInfo;
+import api.PowerBank.ApiHelp.GetToken;
+import api.ReqresSitePractice.Specifications;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class Ep4_2 {
+
+    private final static String URL = "http://172.17.1.45:7254/api/v1";
+
+    @Test
+    public void EP4_1GetInfoAboutBankCardProductsTest() {
+        //Пред установки и пред проверка запроса на статус ответа
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
+
+        //класс в котором у нас лежит метод по получению access token
+        GetToken getToken = new GetToken();
+
+//        Данные для авторизации:
+//
+//        Телефон: 76666666666
+//        Пароль: Ihave6Cards!
+
+        String accessToken = getToken.accessToken("76666666666", "Ihave6Cards!");
+//      accessToken передаем в header() вместе с доп заголовками: "Authorization", "Bearer "
+
+        List<CardProductsInfo> cardProductsInfo  = (
+                given()
+                        .param("isActive","true")
+                        //подставляем данные header
+                        .header("Authorization", "Bearer " + accessToken)
+                        //подставляем данные body
+                        .when()
+                        // указываем endpoint и HTTP метод
+                        .get("/card/products")
+                        .then()
+                        .log()
+                        .all()
+
+                        //извлекаем ответ в класс
+                        .extract()
+                        .body()
+                        .jsonPath()
+                        //в пути ставим точку так как нет явного открытия массива в теле ответа
+                        .getList(".", CardProductsInfo.class)
+        );
+
+        //Проверяем количество карт
+        Assertions.assertEquals(cardProductsInfo.size(),10);
+    }
+
+    @Test
+    public void EP4_1GetInfoAboutShoppingCardProductErrorTest() {
+        //Пред установки и пред проверка запроса на статус ответа
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
+
+        //класс в котором у нас лежит метод по получению access token
+        GetToken getToken = new GetToken();
+        String card = "/Shopping%20Card/";
+
+//        Данные для авторизации:
+//
+//        Телефон: 76666666666
+//        Пароль: Ihave6Cards!
+
+        String accessToken = getToken.accessToken("76666666666", "Ihave6Cards!");
+//      accessToken передаем в header() вместе с доп заголовками: "Authorization", "Bearer "
+
+        List <CardProductInfo> cardProductInfo  = (given()
+                .param("isActive", "true")
+                //подставляем данные header
+                .header("Authorization", "Bearer " + accessToken)
+                //подставляем данные body
+                .when()
+                // указываем endpoint и HTTP метод
+                .get("/card/products" + card)
+                .then()
+                .log()
+                .all()
+
+                //извлекаем ответ в класс
+                .extract()
+                .body()
+                .jsonPath()
+                .getList(".", CardProductInfo.class)
+
+                // Ошибка в тесте возникает потому что мы неправильно определяем тип получаемого ответа и хотим записать его в список,
+                // когда тело ответа содержит объект типа LinkedHashMap, поэтому следующий тест будет иметь правильный вид
+                //где мы присваиваем ответ объекту, без явного определения коллекции List : CardProductInfo cardProductInfo  = (given()...
+        );
+
+        //Проверяем название карты и ее тип
+    }
+    @Test
+    public void EP4_1GetInfoAboutShoppingCardProductTest() {
+        //Пред установки и пред проверка запроса на статус ответа
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
+
+        //класс в котором у нас лежит метод по получению access token
+        GetToken getToken = new GetToken();
+        String card = "/Shopping%20Card/";
+
+//        Данные для авторизации:
+//
+//        Телефон: 76666666666
+//        Пароль: Ihave6Cards!
+
+        String accessToken = getToken.accessToken("76666666666", "Ihave6Cards!");
+//      accessToken передаем в header() вместе с доп заголовками: "Authorization", "Bearer "
+
+        CardProductInfo cardProductInfo  = (given()
+                .param("isActive", "true")
+                //подставляем данные header
+                .header("Authorization", "Bearer " + accessToken)
+                //подставляем данные body
+                .when()
+                // указываем endpoint и HTTP метод
+                .get("/card/products" + card)
+                .then()
+                .log()
+                .all()
+
+                //извлекаем ответ в класс
+                .extract()
+                .body()
+                //в пути ставим точку так как нет явного открытия массива в теле ответа
+                .as( CardProductInfo.class)
+        );
+
+        //Проверяем название карты и ее тип
+        cardProductInfo.getName().equals("Shopping Card");
+        cardProductInfo.getType().equals("credit");
+    }
+}
