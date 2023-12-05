d4fcfa74a643da55ec127ecb4f0b6c03e9760359
Alenin Andrew <aleninmailbox@gmail.com>
2023-12-05 13:26:51 +0300

    edit api

--- src/test/java/api/PowerBank/tests/depositservice/EP3_1/EP3_1.java
+++ src/test/java/api/PowerBank/tests/depositservice/EP3_1/EP3_1.java
@@ -1,9 +1,9 @@
 package api.PowerBank.tests.depositservice.EP3_1;
 
-import api.PowerBank.ApiHelp.ApiRequests;
 import api.PowerBank.ApiHelp.DepositService.DepositsInfo;
 import api.PowerBank.ApiHelp.GetToken;
 import api.PowerBank.ApiHelp.Specifications;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,12 +12,13 @@ import java.util.List;
 import java.util.Map;
 
 import static api.PowerBank.ApiHelp.ApiRequests.getDepositListRequest;
-import static api.PowerBank.ApiHelp.Specifications.URL;
+import static api.PowerBank.ApiHelp.ApiRequests.getRequestP;
+import static api.PowerBank.ApiHelp.Specifications.*;
 
 public class EP3_1 {
 
     @Test
-    public void CleanEP3_1GetInfoAboutDepositProducts1Test() {
+    public void CleanEP3_1GetInfoAboutDepositProductsTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
 
         GetToken getToken = new GetToken();
@@ -31,29 +32,10 @@ public class EP3_1 {
         paramsMap.put("isActive", "true");
         headersMap.put("Authorization", "Bearer " + accessToken);
 
-        DepositsInfo depositsInfo = ApiRequests.getRequestP(paramsMap, headersMap, endPoint).as(DepositsInfo.class);
+        List<DepositsInfo> depositsInfo;
+        Response response = getRequestP(paramsMap, headersMap, endPoint);
 
-        //Проверяем количество депозитных карт банка
-        System.out.println(depositsInfo.getName());
-
-    }
-
-    @Test
-    public void CleanEP3_1GetInfoAboutDepositProducts2Test() {
-        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
-
-        GetToken getToken = new GetToken();
-        String accessToken = getToken.accessToken("79772345685", "ls23Ghq#wEr");
-
-        String endPoint = "/deposit/products";
-
-        Map<String, String> paramsMap = new HashMap<>();
-        Map<String, String> headersMap = new HashMap<>();
-
-        paramsMap.put("isActive", "true");
-        headersMap.put("Authorization", "Bearer " + accessToken);
-
-        List<DepositsInfo> depositsInfo = getDepositListRequest(paramsMap, headersMap, endPoint);
+        depositsInfo = response.getBody().jsonPath().getList(".", DepositsInfo.class);
 
         Assertions.assertEquals(9,depositsInfo.size());
     }
@@ -73,7 +55,10 @@ public class EP3_1 {
         paramsMap.put("isActive", "false");
         headersMap.put("Authorization", "Bearer " + accessToken);
 
-        List<DepositsInfo> depositsInfo = getDepositListRequest(paramsMap, headersMap, endPoint);
+        List<DepositsInfo> depositsInfo;
+        Response response = getRequestP(paramsMap, headersMap, endPoint);
+
+        depositsInfo = response.getBody().jsonPath().getList(".", DepositsInfo.class);
 
         Assertions.assertEquals(1,depositsInfo.size());
         Assertions.assertEquals("AutumnOffer",depositsInfo.get(0).getName());
@@ -81,7 +66,7 @@ public class EP3_1 {
 
     @Test
     public void CleanEP3_1GetInfoAboutDepositProductsIsActiveNotBooleanTest() {
-        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecError400());
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
 
         GetToken getToken = new GetToken();
         String accessToken = getToken.accessToken("79772345685", "ls23Ghq#wEr");
@@ -93,7 +78,8 @@ public class EP3_1 {
         paramsMap.put("isActive", "12");
         headersMap.put("Authorization", "Bearer " + accessToken);
 
-        ApiRequests.getRequestP(paramsMap, headersMap, endPoint);
+        checkStatusCode(responseSpecError400());
+        getRequestP(paramsMap, headersMap, endPoint);
     }
 
     @Test
@@ -107,7 +93,7 @@ public class EP3_1 {
         paramsMap.put("isActive", "true");
         headersMap.put("Authorization", "Bearer");
 
-        ApiRequests.getRequestP(paramsMap, headersMap, endPoint);
+        getRequestP(paramsMap, headersMap, endPoint);
     }
 
 
